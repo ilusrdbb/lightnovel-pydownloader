91892d5b32e7cb0a73b2ed401e22ae890e1ad4eb
xuruian <1925134362@qq.com>
2022-12-09 10:26:58 +0800

    合并txt的粗糙小工具

---
+++ other/mergetxt.py
@@ -0,0 +1,23 @@
+import os
+
+if __name__ == '__main__':
+    """
+    合并txt的粗糙小工具，做epub啥的建议用现成的轮子
+    """
+    book_path = '../books/lightnovel/[とまとすぱげてぃ][web]转生成夹在百合中间的男人了 第六章 你觉得你在百合游戏世界里还能说出同样的话吗？③_3195_'
+    book_name = book_path.split('/')[-1]
+    out_path = '../books/' + book_name + '.txt'
+    file_list = os.listdir(book_path)
+    # 创建时间正序排序
+    new_file_list = sorted(file_list, key=lambda file: os.path.getctime(os.path.join(book_path, file)))
+    # 合并
+    with open(out_path, 'w', encoding='utf-8') as f:
+        for filename in new_file_list:
+            filepath = book_path + '/' + filename
+            # 遍历单个文件，读取行数
+            with open(filepath, encoding='utf-8') as ff:
+                txt = ff.read()
+                f.write(txt)
+            f.write('\n')
+            f.write('-------------------------------------------')
+            f.write('\n')
