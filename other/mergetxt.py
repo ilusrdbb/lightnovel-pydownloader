646037e45338e197c36a330d33c5504b53078173
xuruian <1925134362@qq.com>
2022-12-13 11:25:50 +0800

    工具更新

--- other/mergetxt.py
+++ other/mergetxt.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     """
     合并txt的粗糙小工具，做epub啥的建议用现成的轮子
     """
-    book_path = '../books/lightnovel/[とまとすぱげてぃ][web]转生成夹在百合中间的男人了 第六章 你觉得你在百合游戏世界里还能说出同样的话吗？③_3195_'
+    book_path = '../books/masiro/TS少女的赎罪~回溯原勇者是勇者小队的女剑士~'
     book_name = book_path.split('/')[-1]
     out_path = '../books/' + book_name + '.txt'
     file_list = os.listdir(book_path)
@@ -18,10 +18,11 @@ if __name__ == '__main__':
     # 合并
     with open(out_path, 'w', encoding='utf-8') as f:
         for filename in new_file_list:
-            filepath = book_path + '/' + filename
-            with open(filepath, encoding='utf-8') as ff:
-                txt = ff.read()
-                f.write(txt)
-            f.write('\n')
-            f.write('-------------------------------------------')
-            f.write('\n')
+            if '.txt' in filename:
+                filepath = book_path + '/' + filename
+                with open(filepath, encoding='utf-8') as ff:
+                    txt = ff.read()
+                    f.write(txt)
+                f.write('\n')
+                f.write('-------------------------------------------')
+                f.write('\n')
