cdeb0889df71382bebc6cb46fb37ee7040fab43d
xuruian <1925134362@qq.com>
2023-05-26 13:42:31 +0800

    弱智bug

--- service/login.py
+++ service/login.py
@@ -69,10 +69,10 @@ async def masiro_get_token(login_info, session):
 async def login(login_info, session):
     if login_info.site == 'masiro':
         # 真白萌设置token
-        await login.masiro_get_token(login_info, session)
+        await masiro_get_token(login_info, session)
     if login_info.site == 'oldlightnovel':
         # 旧轻国获取hash
-        await login.oldlightnovel_get_hash(login_info, session)
+        await oldlightnovel_get_hash(login_info, session)
     login_param = build_login_param(login_info)
     login_headers = build_login_headers(login_info)
     if login_info.site == 'oldlightnovel':
