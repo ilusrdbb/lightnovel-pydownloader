21b7bd62297dbe6ca3b87ade51cbe5664cc137d1
xuruian <1925134362@qq.com>
2024-08-27 13:51:04 +0800

    版本号

--- lightnovel.py
+++ lightnovel.py
@@ -23,7 +23,7 @@ def run():
 
 
 if __name__ == '__main__':
-    print("Version 2.0.7")
+    print("Version 2.0.8")
     script.init_db()
     if config.read("scheduler_config")["enabled"]:
         # 添加定时任务
