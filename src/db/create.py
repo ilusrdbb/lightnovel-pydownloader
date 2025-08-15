c5d631066d9047b721ddc7d4017c69bfb22cea15
xura <1925134362@qq.com>
2025-08-15 10:04:54 +0800

    sqlite开启wal，优化多线程性能

--- src/db/create.py
+++ src/db/create.py
@@ -1,6 +1,12 @@
+from sqlalchemy import text
+
 from src.db import BaseDB, engine
 
 
 async def init_db():
+    async with engine.connect() as conn:
+        # 非事务的执行WAL
+        await conn.execute(text("PRAGMA journal_mode=WAL;"))
     async with engine.begin() as conn:
+        # 建表
         await conn.run_sync(BaseDB.metadata.create_all)
