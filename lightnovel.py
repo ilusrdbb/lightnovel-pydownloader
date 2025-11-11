665839ee7774bac41b1091493d1bef4d6fc8ccc3
xura <1925134362@qq.com>
2025-11-11 08:51:39 +0800

    解决轻国下载图片失败的问题 Issue #16

--- lightnovel.py
+++ lightnovel.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     load_config()
     # 初始化日志
     log.init_log()
-    log.info(f"lightnovel-pydownloader version 3.0.9")
+    log.info(f"lightnovel-pydownloader version 3.0.10")
     try:
         asyncio.run(main_async())
     except Exception as e:
